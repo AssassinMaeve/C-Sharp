7bc9901597527216d7b0a507ddfb8c795f437ef6
Maeve Fernandes <maeve.fernandes.7@gmail.com>
2025-09-19 10:13:01 +0530

    Web-Service

---
+++ ASMX/ASMX/WebForm1.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ASMX.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        Number-1:&nbsp;
+        <asp:TextBox ID="NumBox1" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Number-2:&nbsp;
+        <asp:TextBox ID="NumBox2" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Add" OnClick="AddButton_Click" Width="100px" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button2" runat="server" Text="Sub" OnClick="SubButton_Click" Width="101px" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button3" runat="server" Text="Mul" OnClick="MulButton_Click" Width="101px" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button4" runat="server" Text="Div" OnClick="DivButton_Click" Width="101px" />
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="The Result will be displayed here"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ ASMX/ASMX/WebForm1.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ASMX.ServiceReference2;
+
+namespace ASMX
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void AddButton_Click(object sender, EventArgs e)
+        {
+            WebService1 service1 = new WebService1();
+            int a = Convert.ToInt32(NumBox1.Text);
+            int b = Convert.ToInt32(NumBox2.Text);
+            int result = service1.Add(a,b);
+            Label1.Text = result.ToString();
+        }
+        protected void SubButton_Click(object sender, EventArgs e)
+        {
+            WebService1 service1 = new WebService1();
+            int a = Convert.ToInt32(NumBox1.Text);
+            int b = Convert.ToInt32(NumBox2.Text);
+            int result = service1.Sub(a,b);
+            Label1.Text = result.ToString();
+
+        }
+
+        protected void MulButton_Click(object sender, EventArgs e)
+        {
+            // This is one method to call the web service using the connected service reference
+            WebService1SoapClient service1 = new WebService1SoapClient();
+            int a = Convert.ToInt32(NumBox1.Text);
+            int b = Convert.ToInt32(NumBox2.Text);
+            int result = service1.Mul(a,b);
+            Label1.Text = result.ToString();
+
+        }
+
+        protected void DivButton_Click(object sender, EventArgs e)
+        {
+            // This is another method to call the web service using the connected service reference
+            ServiceReference2.WebService1SoapClient service1 = new ServiceReference2.WebService1SoapClient();
+            int a = Convert.ToInt32(NumBox1.Text);
+            int b = Convert.ToInt32(NumBox2.Text);
+            int result = service1.Div(a,b);
+            Label1.Text = result.ToString();
+
+        }
+    }
+}
