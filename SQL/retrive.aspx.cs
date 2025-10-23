11bacd58602d0194df68b64ebee463bd7941b5fb
Maeve Fernandes <maeve.fernandes.7@gmail.com>
2025-10-23 15:17:43 +0530

    SQL Linq Program

---
+++ SQL/retrive.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SQL
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        DataClasses1DataContext ds = new DataClasses1DataContext();
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["username"] == null)
+            {
+                Response.Redirect("login.aspx");
+            }
+        }
+
+
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
